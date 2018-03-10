b813a8811d12084c0869927c392d7ceeb182f6f3
Pankaj Patel <p.patel@holidaypirates.com>
2018-03-10 23:28:10 +0100

    iterate over arrays

--- src/tree-component/tree-component.js
+++ src/tree-component/tree-component.js
@@ -1,18 +1,22 @@
 require('../tree-node/tree-node');
 const css = require('./tree-component.css');
 
+const node = (scope) => `<tree-component data='${JSON.stringify(scope.item)}'></tree-component>`;
+
 const template = (scope) => `
 <style>${css}</style>
-<ul class="tree">
-  <li><small>No Node</small></li>
-</ul>`;
+${scope.data instanceof Array ?
+  `<ul class="tree">${scope.data.map(item => `<li><small>${node({item})}</small></li>`).join('')}</ul>`
+  : scope.data
+}`;
 
 class Tree extends HTMLElement {
   connectedCallback(){
+    this.data = JSON.parse(this.getAttribute('data'));
     this.render();
   }
   render() {
-    this.innerHTML = template();
+    this.innerHTML = template({data: this.data});
   }
 }
 customElements.define('tree-component', Tree);
