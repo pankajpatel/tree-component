95542a8fc547c9cbf78ea0dca292d3d1e5c482dc
Pankaj Patel <p.patel@holidaypirates.com>
2018-03-11 10:51:27 +0100

    expandable nodes

--- src/tree-component/tree-component.js
+++ src/tree-component/tree-component.js
@@ -1,12 +1,18 @@
 require('../tree-node/tree-node');
 const css = require('./tree-component.css');
 
-const node = (scope) => `<tree-component data='${JSON.stringify(scope.item)}'></tree-component>`;
-
+const node = (scope) => `
+<tree-component data='${JSON.stringify(scope.item)}'></tree-component>
+`;
+const label = (data) => {
+  return data instanceof Array ? `[${[].concat(data).join(', ')}]` : data
+}
 const template = (scope) => `
 <style>${css}</style>
-${scope.data instanceof Array ?
-  `<ul class="tree">${scope.data.map(item => `<li><small>${node({item})}</small></li>`).join('')}</ul>`
+${scope.data instanceof Array
+  ? `<ul class="tree">
+      ${scope.data.map(item => `<li class="collapsed"><small>${label(item)}</small>${node({item})}</li>`).join('')}
+    </ul>`
   : scope.data
 }`;
 
@@ -14,6 +20,10 @@ class Tree extends HTMLElement {
   connectedCallback(){
     this.data = JSON.parse(this.getAttribute('data'));
     this.render();
+    this.addEventListener('click', (e) => {
+      e.target.parentElement.classList.toggle('collapsed');
+      e.target.parentElement.classList.toggle('expanded');
+    })
   }
   render() {
     this.innerHTML = template({data: this.data});
